21c9117ed1ec4663d4a47b0e703c3d8fefe1b58e
yitsou1985 <57088413+yitsou1985@users.noreply.github.com>
2021-06-22 19:28:12 +0300

    Updates

---
+++ precache-manifest.04bbbc5f40109aeae775f9949eb6d834.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feb493df29b10dded567adbc5371a9ea",
+    "url": "/Portofolio_JS/index.html"
+  },
+  {
+    "revision": "2f1a3cb2e87bc279a3fa",
+    "url": "/Portofolio_JS/static/css/3.be04442c.chunk.css"
+  },
+  {
+    "revision": "fde22d9416399242ca95",
+    "url": "/Portofolio_JS/static/css/main.b7639457.chunk.css"
+  },
+  {
+    "revision": "a46654abf5b04774c242",
+    "url": "/Portofolio_JS/static/js/2.a653791f.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/Portofolio_JS/static/js/2.a653791f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f1a3cb2e87bc279a3fa",
+    "url": "/Portofolio_JS/static/js/3.feb0a85f.chunk.js"
+  },
+  {
+    "revision": "b3cdbaec999d0d9cdb29e5089b49e575",
+    "url": "/Portofolio_JS/static/js/3.feb0a85f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9b1308d42eaf407bfaf",
+    "url": "/Portofolio_JS/static/js/4.d1dca69d.chunk.js"
+  },
+  {
+    "revision": "312b03fc2032f4a26db5e85de50ce98a",
+    "url": "/Portofolio_JS/static/js/4.d1dca69d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ae6f15de22d0f6ad824",
+    "url": "/Portofolio_JS/static/js/5.907e739c.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/Portofolio_JS/static/js/5.907e739c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fde22d9416399242ca95",
+    "url": "/Portofolio_JS/static/js/main.1366e13c.chunk.js"
+  },
+  {
+    "revision": "aecaaafda095036da709",
+    "url": "/Portofolio_JS/static/js/runtime-main.cfa969b2.js"
+  },
+  {
+    "revision": "3c9ecabeb9bb58358ffc2626b02107a7",
+    "url": "/Portofolio_JS/static/media/CV.3c9ecabe.png"
+  },
+  {
+    "revision": "43f4cedefafc5f54656662164d0ba612",
+    "url": "/Portofolio_JS/static/media/CVPhoto.43f4cede.png"
+  },
+  {
+    "revision": "9b79b0c070b36829bbc3f557fee0da64",
+    "url": "/Portofolio_JS/static/media/Edit_Delete.9b79b0c0.png"
+  },
+  {
+    "revision": "54a4be687831d1ac236c60b57087ed0c",
+    "url": "/Portofolio_JS/static/media/aqua.54a4be68.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Portofolio_JS/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2766bc7ad4e6f4a31bdd5816c7d3c52b",
+    "url": "/Portofolio_JS/static/media/photo_profil.2766bc7a.jpeg"
+  }
+]);
